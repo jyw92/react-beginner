e529295c5172bd2645e353641e64ae31417e8a69
jyw92 <koreajyy@gmail.com>
2025-12-15 23:43:51 +0900

    버그수정22

--- src/Pages/topics/[id]/create.tsx
+++ src/Pages/topics/[id]/create.tsx
@@ -99,7 +99,7 @@ export default function CreateTopic() {
           content: JSON.stringify(content),
           category,
           thumbnail: thumbnailUrl,
-          author: user.id,
+          author: user!.id,
           status: TOPIC_STATUS.TEMP,
         },
       ])
@@ -153,7 +153,7 @@ export default function CreateTopic() {
           content: JSON.stringify(content),
           category,
           thumbnail: thumbnailUrl,
-          author: user.id,
+          author: user!.id,
           status: TOPIC_STATUS.PUBLISH,
         },
       ])
